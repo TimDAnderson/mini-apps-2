09dc2514d2252321283a3f84307db908c996b41b
Tim Anderson <tdanderson805@gmail.com>
2021-06-22 21:11:45 -0700

    wrap up app 2, init setup for app 3

---
+++ challenge_3/client/components/App.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import styled from "styled-components";
+import { ScoreInput } from './ScoreInput'
+import { ScoreBoard } from './ScoreBoard'
+
+export const App = () => {
+
+  return (
+    <div>
+      hi
+      <ScoreInput />
+      <ScoreBoard />
+    </div>
+  )
+}
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
 
+app.use(express.static('public'))
 
+app.get('/hi', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Bowling app listening at http://localhost:${port}`)
+})
