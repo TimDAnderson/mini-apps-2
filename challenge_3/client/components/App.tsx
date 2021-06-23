167b29d6a6b706f8d7651abf872b2f565bb6b15b
Tim Anderson <tdanderson805@gmail.com>
2021-06-22 22:25:48 -0700

    adding bowling init state

--- challenge_3/client/components/App.tsx
+++ challenge_3/client/components/App.tsx
@@ -5,11 +5,58 @@ import { ScoreBoard } from './ScoreBoard'
 
 export const App = () => {
 
+  var [score, setScore] = React.useState<Number>(0);
+  var [roll, setRoll] = React.useState<Number>(1);
+  var [currentFrame, setCurrentFrame] = React.useState<Number>(1);
+  var [frames, setFrames] = React.useState<Array<Object>>([
+    {
+      frame: 1,
+      score: 0,
+    },
+    {
+      frame: 2,
+      score: 0,
+    },
+    {
+      frame: 3,
+      score: 0,
+    },
+    {
+      frame: 4,
+      score: 0,
+    },
+    {
+      frame: 5,
+      score: 0,
+    },
+    {
+      frame: 6,
+      score: 0,
+    },
+    {
+      frame: 7,
+      score: 0,
+    },
+    {
+      frame: 8,
+      score: 0,
+    },
+    {
+      frame: 9,
+      score: 0,
+    },
+    {
+      frame: 10,
+      score: 0,
+    },
+  ]);
+
   return (
     <div>
-      hi
+      Welcome to the game
       <ScoreInput />
       <ScoreBoard />
+      Current Score is {score}
     </div>
   )
 }
--- challenge_3/client/components/ScoreInput.tsx
+++ challenge_3/client/components/ScoreInput.tsx
@@ -4,7 +4,30 @@ export const ScoreInput = props => {
 
   return (
     <div>
-      keypad
+      <table>
+        <tbody>
+          <tr>
+            <td onClick={()=>{}}>1</td>
+            <td onClick={()=>{}}>2</td>
+            <td onClick={()=>{}}>3</td>
+          </tr>
+          <tr>
+            <td onClick={()=>{}}>4</td>
+            <td onClick={()=>{}}>5</td>
+            <td onClick={()=>{}}>6</td>
+          </tr>
+          <tr>
+            <td onClick={()=>{}}>7</td>
+            <td onClick={()=>{}}>8</td>
+            <td onClick={()=>{}}>9</td>
+          </tr>
+          <tr>
+            <td onClick={()=>{}}>X</td>
+            <td> </td>
+            <td onClick={()=>{}}>0</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
