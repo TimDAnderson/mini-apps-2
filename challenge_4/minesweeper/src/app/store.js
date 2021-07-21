91f29964b856f6ce02810dc38a338d03fe469612
Tim Anderson <tdanderson805@gmail.com>
2021-07-20 20:37:54 -0700

    removed temp chart and cleaned up some of the redux logic for minesweeper

--- challenge_4/minesweeper/src/app/store.js
+++ challenge_4/minesweeper/src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import boardReducer from '../features/counter/boardSlice';
-import counterReducer from '../features/counter/counterSlice';
+import boardReducer from '../features/board/boardSlice';
 
 export const store = configureStore({
   reducer: {
     board: boardReducer,
-    counter: counterReducer,
   },
 });
